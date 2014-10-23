41348c7baf6d629c79e4efb73bc1809712f8f000
Evgeny Vilkov <zzeneg@gmail.com>
2014-10-23 17:37:11 +0200

    Add sample hbs templates

--- app.js
+++ app.js
@@ -4,5 +4,56 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-    res.end('Hi there!');
+    res.render('index');
 });
+
+app.get('/simple', function (req, res) {
+    var data = { name: 'Gorilla' };
+    res.render('simple', data);
+});
+
+app.get('/complex', function (req, res) {
+    var data = {
+        name: 'Gorilla',
+        address: {
+            streetName: 'Broadway',
+            streetNumber: '721',
+            floor: 4,
+            addressType: {
+                typeName: 'residential'
+            }
+        }
+    };
+    res.render('complex', data);
+});
+
+app.get('/loop', function (req, res) {
+    var basketballPlayers = [
+        { name: 'Lebron James', team: 'the Heat' },
+        { name: 'Kevin Durant', team: 'the Thunder' },
+        { name: 'Kobe Jordan', team: 'the Lakers' }
+    ];
+
+    var days = [
+        'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+    ];
+
+    var data = {
+        basketballPlayers: basketballPlayers,
+        days: days
+    };
+
+    res.render('loop', data);
+});
+
+app.get('/logic', function (req, res) {
+    var data = {
+        upIsUp: true,
+        downIsUp: false,
+        skyIsBlue: "yes"
+    };
+
+    res.render('logic', data);
+});
+
+app.listen(3000);
